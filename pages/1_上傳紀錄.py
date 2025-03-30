d675b7695fbf9475dbcde1b3dce19ed973047300
Din1225 <dsianlly718@gmail.com>
2025-03-30 21:02:22 +0800

    fix bug

--- pages/1_上傳紀錄.py
+++ pages/1_上傳紀錄.py
@@ -15,7 +15,7 @@ def get_base64_from_url(url):
     else:
         return None
 
-bg_url = "https://storage.googleapis.com/internet_health/upload_bg3.jpg"
+bg_url = "https://storage.googleapis.com/internet_health/upload_bg2.jpg"
 bg_image_base64 = get_base64_from_url(bg_url)
 
 if bg_image_base64:
