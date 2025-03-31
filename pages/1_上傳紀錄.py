7646cad327a301f29bb14312043818fca43e1375
Din1225 <dsianlly718@gmail.com>
2025-04-01 01:27:45 +0800

    add new pages

--- pages/1_上傳紀錄.py
+++ pages/1_上傳紀錄.py
@@ -80,10 +80,13 @@ with st.form("daily_form", clear_on_submit=True):
     dinner_desc = st.text_input("晚餐食物描述", key="dinner_desc")
     dinner_img = st.file_uploader("晚餐照片", type=["png", "jpg", "jpeg"], key="dinner")
     
+    late_night_desc = st.text_input("宵夜食物描述", key="late_night_desc")
+    late_night_img = st.file_uploader("宵夜照片", type=["png", "jpg", "jpeg"], key="late_night")
+
     sugary_drinks = st.number_input("當天含糖飲料數量", min_value=0, step=1, value=0, help="請輸入當天喝的含糖飲料數量")
     steps = st.number_input("今日步數", min_value=0, step=100, value=0)
     steps_evidence = st.file_uploader("上傳步數證明照片", type=["png", "jpg", "jpeg"], key="steps_evidence")
-    reflection = st.text_area("每日反思", help="請記錄今天的心情、心得或學到的事")
+
     
     submit_daily = st.form_submit_button("提交每日紀錄")
 
@@ -98,12 +101,13 @@ if submit_daily:
         "lunch_desc": lunch_desc,
         "dinner": upload_file_to_gcs(dinner_img, record_date, "dinner"),
         "dinner_desc": dinner_desc,
+        "late_night": upload_file_to_gcs(late_night_img, record_date, "late_night"),
+        "late_night_desc": late_night_desc,
         "sugary_drinks": sugary_drinks,
         "steps": steps,
         "steps_evidence": upload_file_to_gcs(steps_evidence, record_date, "steps"),
         "screen_time": screen_time,
         "screen_evidence": upload_file_to_gcs(screen_evidence, record_date, "screen"),
-        "reflection": reflection
     }
     st.session_state.pending_record = new_record
     st.info("請輸入上傳密碼以確認上傳資料。")
